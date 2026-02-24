0976e75937e1128abdffc90921d3e3cd3497a31f
jonnctrl <202856387+Theyobii@users.noreply.github.com>
2026-02-24 18:50:22 -0400

    add patch & and minimal web to class 3

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,14 +1,22 @@
-import express  from 'express';
+import express from 'express';
 import { randomUUID } from 'node:crypto'
 import { readFile } from 'fs/promises';
-import { validateMovie } from './movies.js'
+import cors from 'cors'
+import { validateMovie, validatePartialMovie } from './movies.js'
 
-const data = await readFile('./movies.json')
-const movies = JSON.parse(data)
+const movies = JSON.parse(await readFile(new URL('./movies.json', import.meta.url)))
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
 
 const app = express()
 app.use(express.json())
-app.disable = ('x-powered-by')
+app.use(cors(corsOptions))
+app.disable('x-powered-by')
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' })
@@ -27,41 +35,53 @@ app.get('/movies', (req, res) => {
 })
 
 app.get('/movies/:id', (req, res) => { //path to regrexp 
-    const { id } = req.params
-    const movie = movies.find(movie => movie.id === id)
-    if (movie) return res.json(movie)
-    res.status(404).json({ message: 'Movie not found' })
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+  res.status(404).json({ message: 'Movie not found' })
 
 })
 
 app.post('/movies', (req, res) => {
-    
-    const result = validateMovie(req.body)
 
-    if (!result.success) {
-        return res.status(400).json({
-            error: JSON.parse(result.error.message)   
-        })
-    }
+  const result = validateMovie(req.body)
 
-    const newMovie = {
-        id: randomUUID,
-        ...result.data
-    }    
-        //Esto no seria REST, pq estamos guardando el estado de la aplicacion en memmoria
-        movies.push(newMovie)
+  if (!result.success) {
+    return res.status(400).json({
+      error: result.error.errors
+    })
+  }
 
-        res.status(201).json(newMovie)
+  const newMovie = {
+    id: randomUUID(),
+    ...result.data
+  }
+  //Esto no seria REST, pq estamos guardando el estado de la aplicacion en memmoria
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
 })
 
 app.patch('/movies/:id', (req, res) => {
-   const { id } = req.params
-   const movieIndex = movies.findIndex(movie => movie.id === id)
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
 
-   if (movieIndex === -1) {
+  if (movieIndex === -1) {
     return res.status(404).json({ message: 'Movie not found' })
-   }
+  }
+  const result = validatePartialMovie(req.body)
 
+  if (!result.success) {
+    return res.status(400).json({
+      error: result.error.errors
+    })
+  }
+  const updatedMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+  movies[movieIndex] = updatedMovie
+  res.json(updatedMovie)
 })
 
 const PORT = process.env.PORT || 3000
